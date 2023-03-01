657ed96e9ad20df11ddf846b49b37c45b8f5448e
Pierre SIMOND <70819571+swiipius@users.noreply.github.com>
2023-03-01 14:28:02 +0100

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="theme-color" content="rgb(36,36,36)" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
